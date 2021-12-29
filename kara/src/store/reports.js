18c389eba6cad2cba55f4c4668b02afd91dde893
reddington12 <37213559+Reddington12@users.noreply.github.com>
2021-12-29 14:35:40 +0100

    fix

--- kara/src/store/reports.js
+++ kara/src/store/reports.js
@@ -29,12 +29,13 @@ export default {
     },
     mutations: {
       setReports: (state, payload) => (state.reports = payload),
-    updateMessage: (state, payload) => (state.reports = payload)
-   
+    updateMessage: (state) => (serialNum) => {
+      console.log(state.reports)
+    return state.reports.find(x => x.serialNum === serialNum)
     },
 }
     
     
 
 
-
+};
